044e7c9983112847c96d3439efc4f355aec5e118
Thainara Carvalho <thai.cds1@gmail.com>
2023-05-16 17:56:05 -0300

    aula 2

--- lib/main.dart
+++ lib/main.dart
@@ -15,46 +15,35 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Stack(
-            alignment: AlignmentDirectional.center,
-            
-            children: [
-              Container(color: Colors.red, width: 100, height: 100,),
-              Container(color: Colors.blue, width: 50, height: 50,),
-            ],
+      home:Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter: Meus primeiros passos'),
           ),
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              Container(color: Colors.blue, width: 100, height: 100,),
-              Container(color: Colors.red, width: 50, height: 50,),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(color: Colors.cyan,width: 50, height: 50,),
-              Container(color: Colors.pinkAccent, height: 50, width: 50,),
-              Container(color: Colors.purple, height: 50, width: 50,),
-            ],
-          ),
-          Container(color: Colors.amber, height: 30, width: 300,
-          child: Text('Diamante Amarelo', style: TextStyle(color: Colors.black, fontSize: 28), textAlign: TextAlign.center,),
-          ),
-          ElevatedButton(
-            onPressed: (){
-              print('Você apertou o botão');
-            },
-             child: Text('Aperte o botão!'),
+          body: Container(
+            child: Stack( 
+              children: [
+                Container(color: Colors.blue, height: 140,),
+                Container(
+                  color: Colors.white,
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        color: Colors.black26,
+                        width: 72,
+                        height: 100,
+                      ),
+                      Text('Aprender Flutter'),
+                      ElevatedButton(onPressed: (){}, child: Icon(Icons.arrow_drop_up))
+                    ],
+                  ),)
+              ],
             ),
-         
-      ],
-      ),
+          ),
+          floatingActionButton: FloatingActionButton(onPressed: () {} 
+          ),
+         ),
     );
   }
 }
